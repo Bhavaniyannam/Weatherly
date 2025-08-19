9619de40d4dfd16a94befcac1ed7a4e4dc671b62
YANNAM BHAVANI <bhavaniyannam443@gmail.com>
2025-08-19 12:26:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Weatherly", layout="centered")
+
+with open("Weatherly.html", "r", encoding="utf-8") as file:
+    html_content = file.read()
+
+st.markdown(html_content, unsafe_allow_html=True)
