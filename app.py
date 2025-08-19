b3f2a34fd4e44a2a9115ff1a692378c80c295bb9
YANNAM BHAVANI <bhavaniyannam443@gmail.com>
2025-08-19 16:05:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Weatherly", layout="centered")
 
-with open("Weatherly.html", "r", encoding="utf-8") as file:
+with open("index.html", "r", encoding="utf-8") as file:
     html_content = file.read()
 
 st.markdown(html_content, unsafe_allow_html=True)
